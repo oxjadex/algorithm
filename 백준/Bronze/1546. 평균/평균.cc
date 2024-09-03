6c21c693f4d42f42402c570dd12a9d30da4030de
오아린 <128698348+oxjadex@users.noreply.github.com>
2024-09-04 08:21:52 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -1,31 +1,19 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> // std::max_element
+#include <algorithm>
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    
-    vector<int> scores(n);
-    for (int i = 0; i < n; i++) {
-        cin >> scores[i];
+    int a, max=0;
+    double b=0;
+    cin >> a;
+    vector <int> v(a);
+
+    for (int i = 0; i < a; i++) {
+        cin >> v[i];
+        if (v[i]>max) max=v[i];
+        b+=v[i];
     }
-
-    // 최대값 구하기
-    int max_score = *max_element(scores.begin(), scores.end());
-
-    // 조정된 점수의 합 계산
-    double sum_adjusted_scores = 0.0;
-    for (int i = 0; i < n; i++) {
-        sum_adjusted_scores += (double)scores[i] / max_score * 100;
-    }
-
-    // 조정된 점수의 평균 계산
-    double average = sum_adjusted_scores / n;
-
-    // 평균 출력
-    cout << average << endl;
-
+    cout << ((b / max) / a) * 100 ;
     return 0;
 }
