2330e60ee30d6853600d1229bb4a458bef97de21
오아린 <128698348+oxjadex@users.noreply.github.com>
2024-10-10 08:26:43 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,8 +1,3 @@
 def solution(myString, pat):
     answer = 0
-    reverse = ''
-    dict = {"A": "B", "B": "A"}
-    for i in myString:
-        reverse+=(dict[i]) 
-    print(reverse)
-    return 1 if pat in reverse else 0
+    return answer
